8f8b5a165188a4aa9f7c659144d635fc5ae8c7b4
Jared Sanborn <jaredsanborn@gmail.com>
2025-09-07 09:50:45 -0500

    Revert "rsbuild version"
    This reverts commit da24ec93100d17f8f0111d6f85748801461c2d71.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          hello world
         </p>
         <a
           className="App-link"
