cef60c4e500672b06965c19ca99e55e67c27b9a6
Jared Sanborn <jaredsanborn@gmail.com>
2026-04-24 14:03:51 -0500

    adding row and column components for readability

--- src/components/tools/dice/DiceRoller.tsx
+++ src/components/tools/dice/DiceRoller.tsx
@@ -5,6 +5,15 @@ import { handfull } from '../../../classes/handfull-class';
 import DiceButton from './DiceButton'
 import RollResultsQueue from './RollResultsQueue';
 import SharedButtons from './SharedButtons';
+import { CSSProperties } from 'react';
+
+function Row(props: { children: React.JSX.Element[], style?: CSSProperties }): React.JSX.Element {
+    return <div style={{ display: 'flex', flexDirection: 'row', ...props.style }}>{props.children}</div>
+}
+
+function Column(props: { children: React.JSX.Element[], style?: CSSProperties }): React.JSX.Element {
+    return <div style={{ display: 'flex', flexDirection: 'column', ...props.style }}>{props.children}</div>
+}
 
 function DiceRoller() {
     const { customHandfulls, tempDiceString, tempName } = diceStore;
@@ -15,25 +24,23 @@ function DiceRoller() {
     });
 
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column'
-        }}>
-            <div style={{ display: 'flex', flexDirection: 'column', marginRight: 'auto' }}>
-                <input placeholder={'ex: fire sword or empty'} onChange={(e) => { setHandfullName(e.target.value) }} value={tempName} />
-                <input placeholder={'ex: 1d8+2d4+3'} onChange={(e) => { setTempDiceString(e.target.value) }} value={tempDiceString} />
-            </div>
-            <div className={'dice-button-row'}>
-                <button onClick={rollTempHandfull}>{'roll'}</button>
-                <button onClick={saveHandfull}>{'save'}</button>
-                {/* <button onClick={exportHandfulls}>{'export to clipboard'}</button> */}
-                <SharedButtons/>
-            </div>
-            <RollResultsQueue/>
+        <Column>
+            <Column style={{ marginRight: 'auto' }}>
+                <Row>
+                    <input placeholder={'ex: fire sword or empty'} onChange={(e) => { setHandfullName(e.target.value) }} value={tempName} />
+                    <button onClick={saveHandfull}>{'save'}</button>
+                </Row>
+                <Row>
+                    <input placeholder={'ex: 1d8+2d4+3'} onChange={(e) => { setTempDiceString(e.target.value) }} value={tempDiceString} />
+                    <button onClick={rollTempHandfull}>{'roll'}</button>
+                </Row>
+            </Column>
+            <SharedButtons />
+            <RollResultsQueue />
             <div className={'dice-button-row'}>
                 {buttons}
             </div>
-        </div>
+        </Column>
     );
 }
 
